b8dc30ba537f288018b38f273007b43987fbe132
FabianIAM12 <fabian.b89@gmail.com>
2018-05-10 22:54:45 +0200

    use better vars

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var restify = require('restify');
 var server = restify.createServer();
 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
+mongoose.connect(process.env.MONGODB_URI);
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
