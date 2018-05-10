19aa84f2d49a8c30479db775ea3360fe7f4acaa0
FabianIAM12 <fabian.b89@gmail.com>
2018-05-10 11:36:30 +0200

    improved post function

--- app.js
+++ app.js
@@ -1,35 +1,22 @@
 var restify = require('restify');
-var request = require('request');
-
 var server = restify.createServer();
 
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/test');
 
-/*
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/mydb";
-
-MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-    console.log("Database created!");
-    db.close();
-});
-*/
-
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-    console.log('connected to mongodb');
+	console.log('connected to mongodb');
 });
 
 var wineSchema = mongoose.Schema({
-    id: Number,
-    name: String,
-    year: String,
-    country: String,
-    type: String,
-    description: String
+	id: Number,
+	name: String,
+	year: String,
+	country: String,
+	type: String,
+	description: String
 });
 
 var Wine = mongoose.model('Wine', wineSchema);
@@ -48,158 +35,143 @@ wein.save(function(error, document){ });
 obstler.save(function(error, document){ });
 
 function clean_entry(wine){
-    return({'id':wine.id,
-        'name':wine.name,
-        'year':wine.year,
-        'country':wine.country,
-        'type':wine.type,
-        'description':wine.description})
+	return({'id':wine.id,
+		'name':wine.name,
+		'year':wine.year,
+		'country':wine.country,
+		'type':wine.type,
+		'description':wine.description});
 }
 
 function clean_and_split_parameters(parameters){
-    var parameter;
-    var search_params_array = [];
-    var valid_filters = ['year', 'category', 'name', 'country'];
+	var parameter;
+	var search_params_array = [];
+	var valid_filters = ['year', 'category', 'name', 'country'];
 
-    for (var key in parameters){
-        parameter = parameters[key].replace('&','');
-        if (parameter.includes('&')){
-            var remaining_vars = parameter.split('&');
-            for (var i = 0; i < remaining_vars.length; i++) {
-                search_params_array.push(remaining_vars[i]);
-            }
-        }else if(parameter){
-            search_params_array.push(parameter);
-        }
-    }
-    var result_dict = {};
-    for (var i = 0; i < search_params_array.length; i++) {
-        var entry = search_params_array[i].split('=');
+	for (var key in parameters){
+		parameter = parameters[key].replace('&','');
+		if (parameter.includes('&')){
+			var remaining_vars = parameter.split('&');
+			for (var i = 0; i < remaining_vars.length; i++) {
+				search_params_array.push(remaining_vars[i]);
+			}
+		}else if(parameter){
+			search_params_array.push(parameter);
+		}
+	}
+	var result_dict = {};
+	for (var y = 0; y < search_params_array.length; y++) {
+		var entry = search_params_array[i].split('=');
 
-        if (valid_filters.includes(entry[0])) {
-            result_dict[entry[0]] = entry[1];
-        }
-    }
-    return result_dict
+		if (valid_filters.includes(entry[0])) {
+			result_dict[entry[0]] = entry[1];
+		}
+	}
+	return result_dict;
 }
 
-/*
-server.get(/^\/([a-zA-Z0-9_\.~-]+)\/(.*)/, function(req, res, next) {
-    console.log(req.params[0]);
-    console.log(req.params[1]);
-  */
-
 server.get('/wines', function (req, res) {
-    Wine.find({}, function(err, wines) {
-        var wines_array = [];
-        wines.forEach(function (wine) {
-            wines_array.push(clean_entry(wine));
-        });
-        res.writeHead(200, {"Content-Type": "application/json"});
-        var json = JSON.stringify(wines_array);
-        res.end(json);
-    });
+	Wine.find({}, function(err, wines) {
+		var wines_array = [];
+		wines.forEach(function (wine) {
+			wines_array.push(clean_entry(wine));
+		});
+		res.writeHead(200, {'Content-Type': 'application/json'});
+		var json = JSON.stringify(wines_array);
+		res.end(json);
+	});
 });
 
-server.get(/wines\/([a-zA-Z0-9_=\.~-]+)(.*)/, function(req, res, next) {
-
-    var search_parameters = clean_and_split_parameters(req.params);
-    console.log(search_parameters);
-
-    var query = Wine.find(search_parameters);
-
-    // selecting the `name` and `occupation` fields
-    //query.select('name occupation');
-
-    // execute the query at a later time
-    query.exec(function (err, wine) {
-        if (err) return handleError(err);
-        // Prints "Space Ghost is a talk show host."
-        console.log('%s', wine.name);
-    });
-    //
-
-    /*
-    Wine.find({}, function(err, wines) {
-        var wines_array = [];
-        wines.forEach(function (wine) {
-            wines_array.push(clean_entry(wine));
-        });
-        res.writeHead(200, {"Content-Type": "application/json"});
-        var json = JSON.stringify(wines_array);
-        res.end(json);
-    });*/
-
-    res.send(200);
-    return next();
+server.get(/wines&([a-zA-Z0-9_=\.~-]+)(.*)/, function(req, res, next) {
+	var wines_array = [];
+	var query = Wine.find(clean_and_split_parameters(req.params));
+	query.exec(function (err, wines) {
+		if (err) return handleError(err);
+		wines.forEach(function (wine) {
+			wines_array.push(clean_entry(wine));
+		});
+		res.writeHead(200, {'Content-Type': 'application/json'});
+		var json = JSON.stringify(wines_array);
+		res.end(json);
+	});
 });
 
 server.post('/wines', function (req, res) {
-    var wine_categories = ['red', 'white', 'rose'];
-    var validation_errors = [];
+	var wine_categories = ['red', 'white', 'rose'];
+	var validation_errors = [];
 
-    var POST = {};
-    req.on('data', function(data) {
-        data = data.toString();
-        data = data.split('&');
-        for (var i = 0; i < data.length; i++) {
-            var _data = data[i].split("=");
-            POST[_data[0]] = _data[1];
-        }
+	var POST = {};
+	req.on('data', function(data) {
+		data = data.toString();
+		data = data.split('&');
+		for (var i = 0; i < data.length; i++) {
+			var _data = data[i].split('=');
+			POST[_data[0]] = _data[1];
+		}
 
-        if (!POST['name']){
-            validation_errors.push({'type':'MISSING'});
-        }
-        // TODO: validator name
+		var word_validator = /([a-zA-Z])\w+/;
+		if (!POST['name']){
+			validation_errors.push({'name':'MISSING'});
+		}else if(!word_validator.test(POST['name'])){
+			validation_errors.push({'name':'INVALID'});
+		}
 
-        var year_re = /^\d{4}$/;
-        if (!POST['year']){
-            validation_errors.push({'type':'MISSING'});
-        }else if(!year_re.test(POST['year'])){
-            validation_errors.push({'type':'INVALID'});
-        }
+		var year_re = /^\d{4}$/;
+		if (!POST['year']){
+			validation_errors.push({'year':'MISSING'});
+		}else if(!year_re.test(POST['year'])){
+			validation_errors.push({'year':'INVALID'});
+		}
 
-        if (!POST['country']){
-            validation_errors.push({'type':'MISSING'});
-        }
-        // TODO: validator country
+		if (!POST['country']){
+			validation_errors.push({'country':'MISSING'});
+		}else if(!word_validator.test(POST['country'])){
+			validation_errors.push({'country':'INVALID'});
+		}
 
-        if (!POST['type']){
-            validation_errors.push({'type':'MISSING'});
-        }else if(!wine_categories.includes(POST['type'])){
-            validation_errors.push({'type':'INVALID'});
-        }
+		if (!POST['type']){
+			validation_errors.push({'type':'MISSING'});
+		}else if(!wine_categories.includes(POST['type'])){
+			validation_errors.push({'type':'INVALID'});
+		}
 
-        if (validation_errors.length > 0){
-            res.send({error: 'VALIDATION ERROR',
-                validation: validation_errors});
-        }else{
-            var saufi = new Wine({ id: 10, name: POST['name'], year: POST['year'], country: POST['country'], type: POST['type'], description: POST['description']});
-            saufi.save(function(error, document){ });
-        }
-    });
+		if (validation_errors.length > 0){
+			res.send({error: 'VALIDATION ERROR',
+				validation: validation_errors});
+		}else{
+            Wine.find().limit(1).sort({ id: -1 }).exec(function (err, result) {
+                var next_id = 1;
+                if (result[0].id){
+                    next_id = result[0].id + 1;
+                }
+                var new_entry = new Wine({ id: next_id, name: POST['name'], year: POST['year'], country: POST['country'], type: POST['type'], description: POST['description']});
+                new_entry.save();
+            });
+            res.send({'success': true});
+		}
+	});
 });
 
 server.get('/wines/:id', function (req, res) {
-    Wine.findOne({'id': req.params['id']}).exec(function (err, result) {
-        if (result) {
-            res.send(JSON.stringify(clean_entry(wine)));
-        }else{
-            res.send({error: 'UNKNOWN OBJECT'})
-        }
-    });
+	Wine.findOne({'id': req.params['id']}).exec(function (err, result) {
+		if (result) {
+			res.send(JSON.stringify(clean_entry(wine)));
+		}else{
+			res.send({error: 'UNKNOWN OBJECT'});
+		}
+	});
 });
 
 server.del('/wines/:id', function (req, res) {
-    Wine.findOneAndRemove({'id': req.params['id']}).exec(function (err, result) {
-        if (result) {
-            res.send({'success': true});
-        }else{
-            res.send({error: 'UNKNOWN OBJECT'})
-        }
-    });
+	Wine.findOneAndRemove({'id': req.params['id']}).exec(function (err, result) {
+		if (result) {
+			res.send({'success': true});
+		}else{
+			res.send({error: 'UNKNOWN OBJECT'});
+		}
+	});
 });
 
 server.listen(8080, function() {
-    console.log('%s listening at %s', server.name, server.url);
+	console.log('%s winery startet - listening at %s', server.name, server.url);
 });
